46319c1591148839c1ea12e613f9598bb5280ac5
jack-cook-repo <dbt.udemy.course@gmail.com>
2023-04-04 19:32:28 +0100

    Ephemeral model

--- lessons/models/marts/orders.sql
+++ lessons/models/marts/orders.sql
@@ -14,7 +14,6 @@ order_item_measures AS (
 )
 
 SELECT
-	-- Dimensions from our staging orders table
 	od.order_id,
 	od.created_at AS order_created_at,
 	od.shipped_at AS order_shipped_at,
@@ -22,13 +21,17 @@ SELECT
 	od.returned_at AS order_returned_at,
 	od.status AS order_status,
 	od.num_items_ordered,
-
-	-- Metrics on an order level
 	om.total_sale_price,
 	om.total_product_cost,
 	om.total_profit,
-	om.total_discount
+	om.total_discount,
+
+	-- In practise we'd calculate this column in the model itself, but it's
+	-- a good way to demonstrate how to use an ephemeral materialisation
+	TIMESTAMP_DIFF(od.created_at, user_data.first_order_created_at, DAY) AS days_since_first_order
 
 FROM {{ ref('stg_ecommerce__orders') }} AS od
 LEFT JOIN order_item_measures AS om
 	ON od.order_id = om.order_id
+LEFT JOIN {{ ref('int_ecommerce__first_order_created') }} AS user_data
+	ON od.user_id = user_data.user_id
