24f08b7f9d1d87046c22b97714d61b1fe4e51d82
carloscutillas <carlos.cutillasdefrutos@gmail.com>
2023-05-11 20:23:12 +0100

    finishing data modelling module

---
+++ lessons/models/staging/stg_ecommerce__events.sql
@@ -0,0 +1,41 @@
+{{
+	config(
+		materialized='incremental',
+		unique_key='event_id',
+		on_schema_change='sync_all_columns',
+		partition_by={
+			"field": "created_at",
+			"data_type": "timestamp",
+			"granularity": "day"
+		}
+	)
+}}
+
+WITH source AS (
+	SELECT *
+
+	FROM {{ source('thelook_ecommerce', 'events') }}
+)
+
+SELECT
+	id AS event_id,
+	user_id,
+	sequence_number,
+	session_id,
+	created_at,
+	ip_address,
+	city,
+	state,
+	postal_code,
+	browser,
+	traffic_source,
+	uri AS web_link,
+	event_type
+
+FROM source
+
+{% if is_incremental() %}
+
+WHERE created_at > (SELECT MAX(created_at) FROM {{ this }})
+
+{% endif %}
