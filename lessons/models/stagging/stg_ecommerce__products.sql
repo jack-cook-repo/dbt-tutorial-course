9a5f45bc5b7bbe8da460843754fc9950db2c112e
HristoPanov <hristopanov@gmail.com>
2023-06-30 17:35:25 +0300

    added 3 models - orders,order_items,products

---
+++ lessons/models/stagging/stg_ecommerce__products.sql
@@ -0,0 +1,16 @@
+WITH source AS (
+        SELECT
+			*
+        FROM {{ source('thelook_ecommerce', 'products') }}
+)
+SELECT
+        id AS product_id,
+        cost,
+        category,
+        name,
+        brand,
+        retail_price,
+        department,
+        sku,
+        distribution_center_id
+FROM source
