414c72bd6a00a7e3f8f8104ea7248e585b2f0a45
Tiago.Cunha <tiagocunha61@gmail.com>
2024-05-28 20:58:45 +0200

    add dbt tests

--- dbt_trainning_proj/models/staging/stg_ecommerce__products.sql
+++ dbt_trainning_proj/models/staging/stg_ecommerce__products.sql
@@ -11,6 +11,15 @@ SELECT
 	-- Other columns
 	cost,
 	retail_price,
-	department
+	department,
+	brand -- new column added in v2
+
+	{#- Unused columns:
+		- inventory_item_id
+		- distribution_center_id
+		- category
+		- sku
+		- name
+	#}
 
 FROM source
