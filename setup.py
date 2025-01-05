c006f72e9b64a2f53929223308732960198cee25
yauhenpinchuk <yauhenpinchuk@gmail.com>
2025-01-05 13:50:26 +0000

    copied dagster fiel to root folder

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="lessons_dagster",
     version="0.0.1",
+    working_directory = "lessons_dagster",
     packages=find_packages(),
     package_data={
         "lessons_dagster": [
