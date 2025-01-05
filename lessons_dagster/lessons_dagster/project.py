182eda8e611d0de4a1162066a25ca2ac87a898fb
yauhenpinchuk <yauhenpinchuk@gmail.com>
2025-01-05 21:43:33 +0000

    cnhaged path to dbt project

--- lessons_dagster/lessons_dagster/project.py
+++ lessons_dagster/lessons_dagster/project.py
@@ -3,7 +3,6 @@ from pathlib import Path
 from dagster_dbt import DbtProject
 
 lessons_project = DbtProject(
-    project_dir=Path(__file__).joinpath("..", "..", "lessons").resolve(),
-    packaged_project_dir=Path(__file__).joinpath("..", "..", "dbt-project").resolve(),
+    project_dir=Path(__file__).joinpath("..", "..", '".."',"lessons").resolve(),
 )
 lessons_project.prepare_if_dev()
