04220f8468a594c689bd3eb2958f03af5007fc66
yauhenpinchuk <yauhenpinchuk@gmail.com>
2025-01-05 21:45:49 +0000

    fix

--- lessons_dagster/lessons_dagster/project.py
+++ lessons_dagster/lessons_dagster/project.py
@@ -3,6 +3,6 @@ from pathlib import Path
 from dagster_dbt import DbtProject
 
 lessons_project = DbtProject(
-    project_dir=Path(__file__).joinpath("..", "..", '".."',"lessons").resolve(),
+    project_dir=Path(__file__).joinpath("..", "..", "..","lessons").resolve(),
 )
 lessons_project.prepare_if_dev()
