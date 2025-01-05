3ec54d83ded6c21f511c123556a54163a15eed88
yauhenpinchuk <yauhenpinchuk@gmail.com>
2025-01-05 21:51:01 +0000

    fix consts

--- lessons_dagster/lessons_dagster/constants.py
+++ lessons_dagster/lessons_dagster/constants.py
@@ -1,9 +1,8 @@
 from pathlib import Path
 import os
 
-FILE_PATH = Path(__file__).joinpath("..", "..", "..", "lessons").resolve()
-
 START_DATE = "2024-01-01"
 END_DATE = "2025-04-01"
 
-os.environ['MY_VARIABLE'] = 'value'
+os.environ['DBT_PROFILES_DIR'] = "."
+os.environ['BIGQUERY_PROJECT'] = "dbt-test-445518"
