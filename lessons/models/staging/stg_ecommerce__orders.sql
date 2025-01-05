5187f83f6436ba161743e8745da47bd062c39b10
Raf Carpentero <raf.carpentero@vlaanderen.be>
2025-01-05 19:08:45 +0100

    day1

---
+++ lessons/models/staging/stg_ecommerce__orders.sql
@@ -0,0 +1,17 @@
+WITH source AS (
+	SELECT *
+
+	FROM {{ source('thelook_ecommerce', 'orders') }}
+)
+
+SELECT
+	order_id,
+	user_id,
+	status,
+	created_at,
+	returned_at,
+	shipped_at,
+	delivered_at,
+	num_of_item
+
+FROM source
